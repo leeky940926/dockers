1f1826e75082e0619b030dc7e8b1beef38871833
leeky940926 <leeky940926@gmail.com>
2022-05-09 22:35:34 +0900

    세팅분리

--- dockers/settings/base.py
+++ dockers/settings/base.py
@@ -13,49 +13,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!7l3ad@a)krju&)p^0d*buy$plc0c=mcn25leb%8kt^gsy^6f$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
-CUSTOMIEZED_APPS = [
-    
-]
-
-THIRD_PARTY_APPS = [
-    
-]
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-] + CUSTOMIEZED_APPS + THIRD_PARTY_APPS
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 
 ROOT_URLCONF = 'dockers.urls'
 
---
+++ dockers/settings/installed_apps.py
@@ -0,0 +1,17 @@
+CUSTOMIEZED_APPS = [
+    
+]
+
+THIRD_PARTY_APPS = [
+    'django_extensions',
+    'corsheaders'
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+] + CUSTOMIEZED_APPS + THIRD_PARTY_APPS
