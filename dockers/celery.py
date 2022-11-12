bb33b0e6253d8141d92253e6fd21723d8e9d65ee
leeky940926 <leeky940926@gmail.com>
2022-11-12 18:55:08 +0900

    requirements

--- dockers/celery.py
+++ dockers/celery.py
@@ -1,7 +1,7 @@
+from __future__ import absolute_import,unicode_literals
 import os
 
 from celery import Celery
-from __future__ import absolute_import,unicode_literals
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dockers.settings')
