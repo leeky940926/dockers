00df2ed09ed6870399ee7ab0ba9cd712dc520952
leeky940926 <leeky940926@gmail.com>
2022-11-19 18:13:40 +0900

    eb setting

--- dockers/settings/base.py
+++ dockers/settings/base.py
@@ -110,3 +110,49 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+import os
+import requests
+# Other imports ...
+
+def is_ec2_linux():
+    """
+    Detect if we are running on an EC2 Linux Instance
+    See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")   
+    return False
+
+def get_token():
+    """
+    Set the autorization token to live for 6 hours (maximum)
+    """
+    headers = {
+        'X-aws-ec2-metadata-token-ttl-seconds': '21600',
+    }
+    response = requests.put('http://169.254.169.254/latest/api/token', headers=headers)
+    return response.text
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server.
+    See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
+    """
+
+    if not is_ec2_linux():
+        return None
+    try:
+        token = get_token()
+        headers = {
+            'X-aws-ec2-metadata-token': f"{token}",
+        }
+        response = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', headers=headers)
+        return response.text
+    except:
+        return None
+    finally:
+        if response:
+            response.close()
