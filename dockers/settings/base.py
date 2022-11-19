f6aa22e9ede87f7d85cab61b1f563119ea3a858b
leeky940926 <leeky940926@gmail.com>
2022-11-19 18:15:14 +0900

    eb setting

--- dockers/settings/base.py
+++ dockers/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 ROOT_URLCONF = 'dockers.urls.root'
